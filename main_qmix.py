36510dee0f445f25c60a5cf2fe9c28b1af2cb21f
안규리 <ahnkjuree@gmail.com>
2021-05-04 11:34:10 +0900

    qmix

--- main_qmix.py
+++ main_qmix.py
@@ -2,26 +2,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 import wandb
 
+from datetime import date
 from env_wrapper.sc2_env_wrapper import StarCraft2Env
 from src.agents.Qmixagent import QAgent
 
-use_wandb = False
-test = True
+use_wandb = True
+test = False
 env = StarCraft2Env(map_name="8m", window_size_x=400, window_size_y=300, enemy_obs=False)
 
 state_dim = env.get_obs_size()
 action_dim = env.n_actions - 1
 
-num_episodes = 5000  # goal: 2 million timesteps; 15000 episodes approx.
+num_episodes = 10000  # goal: 2 million timesteps; 15000 episodes approx.
 if test:
     n_agents = 10
 else:
     n_agents = env.n_agents
     if use_wandb:
-        wandb.init(project='optmarl', name='constraint6')
+        wandb.init(project='optmarl', name=date.today().isoformat() + '-Qmix_mac')
 batch_size = 20
 
-agent = QAgent(state_dim, action_dim, n_agents, batch_size=batch_size)
+agent = QAgent(state_dim, action_dim, n_agents, batch_size=batch_size, state_shape=env.get_state_size())
 
 rewards = []
 
@@ -40,13 +41,13 @@ for e in range(num_episodes):
             state = env.get_obs()
             avail_actions = env.get_avail_actions()
 
-        action = agent.get_action(state, avail_actions)
+        env_action, action = agent.get_action(state, avail_actions)
 
         if test:
             reward, terminated = 1, 0
             next_state = np.random.random((n_agents, state_dim))
         else:
-            reward, terminated, _ = env.step(action)
+            reward, terminated, _ = env.step(env_action)
             next_state = env.get_obs()
 
         agent.push(state, action, reward, next_state, terminated, avail_actions)
--- src/agents/Qmixagent.py
+++ src/agents/Qmixagent.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import numpy as np
+import wandb
 
 from torch.optim import Adam
 from collections import namedtuple
@@ -13,8 +14,8 @@ Transition_base = namedtuple('Transition', ('state', 'action', 'reward', 'next_s
 
 
 class QAgent(BaseAgent):
-    def __init__(self, state_dim, action_dim, n_ag, memory_len=10000, batch_size=5, train_start=1000, epsilon=1.0,
-                 epsilon_decay=2 * 1e-5, gamma=0.99, hidden_dim=32, mixer=True, loss_ftn=nn.MSELoss(), lr=1e-4,
+    def __init__(self, state_dim, action_dim, n_ag, memory_len=10000, batch_size=20, train_start=100, epsilon=1.0,
+                 epsilon_decay=2 * 1e-5, gamma=0.99, hidden_dim=32, mixer=False, loss_ftn=nn.MSELoss(), lr=1e-4,
                  state_shape=(0, 0)):
         super(QAgent, self).__init__(state_dim, action_dim, memory_len, batch_size, train_start, gamma)
 
@@ -63,7 +64,19 @@ class QAgent(BaseAgent):
         # anneal epsilon
         self.epsilon = max(self.epsilon - self.epsilon_decay, 0.05)
 
-        return action
+        env_action = self.convert_nn_action_to_env_action(action, avail_actions)
+
+        return env_action, action
+
+    @staticmethod
+    def convert_nn_action_to_env_action(action, avail_actions):
+        dead_ag_loc = avail_actions[:, 0] == 1
+        env_action = action + 1
+        env_action[dead_ag_loc] = 0
+        return env_action
+
+
+
 
     def fit(self):
         samples = self.memory.sample(self.batch_size)
@@ -78,8 +91,8 @@ class QAgent(BaseAgent):
         lst = [s, a, r, ns, t, avail_actions]
 
         for sample in samples:
-            for sam, lst in zip(sample, lst):
-                lst.append(sam)
+            for sam, llst in zip(sample, lst):
+                llst.append(sam)
 
         s_tensor = torch.Tensor(s).to(self.device)
         a_tensor = torch.tensor(a, dtype=int).reshape(-1, 1).to(self.device)
@@ -87,19 +100,22 @@ class QAgent(BaseAgent):
         ns_tensor = torch.Tensor(ns).to(self.device)
         t_tensor = torch.Tensor(t).to(self.device)
 
-        curr_qs = self.get_qs(s_tensor).gather(dim=1,
-                                               index=a_tensor)
+        curr_qs = self.get_qs(s_tensor.reshape(-1, self.state_dim)).gather(dim=1,
+                                               index=a_tensor).reshape(self.batch_size, -1, 1)
 
         with torch.no_grad():
-            next_qs = self.get_qs(ns_tensor).max(dim=1)[0]
+            next_qs = self.get_qs(ns_tensor.reshape(-1, self.state_dim)).max(dim=1)[0].reshape(self.batch_size, -1, 1)
 
         if self.mixer is not None:
-            curr_qs = self.mixer(curr_qs, state=None)
-            next_qs = self.mixer(next_qs, state=None)
+            curr_qs = self.mixer(curr_qs, state=s_tensor)
+            next_qs = self.mixer(next_qs, state=ns_tensor)
 
-        q_target = r_tensor + self.gamma * next_qs * (1 - t_tensor)
+        q_target = r_tensor + self.gamma * next_qs.detach() * (1 - t_tensor)
 
         loss_q = nn.MSELoss()(curr_qs, q_target)
 
         self.optimizer.zero_grad()
         loss_q.backward()
+
+        wandb.log({'loss_critic':loss_q.item()})
+
